599d71b06e7c3b3c8f9f865d50c4fc7b2d367497
fabio_leardini <fabio.leardini@unear.net>
2014-09-19 12:13:20 -0300

    First Commit

---
+++ FileDelete/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string dirPath = @"\\Sr2k3-smlciab2011\d$\Converge\WindowsService\Log\SML - CVG Import\20130809\03";
+                string[] files = Directory.GetFiles(dirPath);
+                int count = Directory.GetFiles(dirPath).Length;
+
+                Console.WriteLine("Quantidade de arquivos a serem deletados: " + count);
+                Console.ReadLine();
+
+                foreach (string f in files)
+                {
+                    File.Delete(f);
+                    count--;
+                    Console.Write("Quantidade de arquivos a serem deletados: " + count);
+                    Console.Out.WriteLine();
+                }
+
+                Console.WriteLine("Fim!");
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
